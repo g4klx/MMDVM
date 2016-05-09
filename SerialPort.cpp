61cacc40b979d0daca52f5db6652b8c7189e818b
Jonathan Naylor <naylorjs@yahoo.com>
2016-05-09 21:58:44 +0100

    Bump the version date.

--- SerialPort.cpp
+++ SerialPort.cpp
@@ -60,7 +60,7 @@ const uint8_t MMDVM_DEBUG3       = 0xF3U;
 const uint8_t MMDVM_DEBUG4       = 0xF4U;
 const uint8_t MMDVM_DEBUG5       = 0xF5U;
 
-const uint8_t HARDWARE[]         = "MMDVM 20160428 24kHz (D-Star/DMR/System Fusion)";
+const uint8_t HARDWARE[]         = "MMDVM 20160509 24kHz (D-Star/DMR/System Fusion/CW Id)";
 
 const uint8_t PROTOCOL_VERSION   = 1U;
 
