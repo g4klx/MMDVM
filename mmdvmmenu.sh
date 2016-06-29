70df22ef8157a351b3eef78c15a08b528d3fb430
phl0 <florian@florian-wolters.de>
2016-06-29 12:40:45 +0200

    Make sed commands more specific

--- mmdvmmenu.sh
+++ mmdvmmenu.sh
@@ -10,23 +10,23 @@
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 #
 ###############################################################################
 #
-# On a Linux based system, such as a Raspberry Pi, this script will perform 
-# Modafacation to the Config.c file for most options. It makes a Back up when 
+# On a Linux based system, such as a Raspberry Pi, this script will perform
+# Modafacation to the Config.c file for most options. It makes a Back up when
 # you staet the script if none is present. You must recompile and load firmwhare
 # on Due if changes are made.
-# 
+#
 ###############################################################################
 #
 #                              CONFIGURATION
@@ -44,7 +44,7 @@ confbak=Config.h.bak
 
 
 
-# Check for backup file and make one if not present 
+# Check for backup file and make one if not present
 
 if [ ! -f $confbak ];then
 
@@ -70,14 +70,14 @@ do
 EOF
     read -n1 -s
     case "$REPLY" in
-    "1")  sed -e 's/\/\/ #define EXTERNAL_OSC 12/ #define EXTERNAL_OSC 12/g' $conf > $conf.tmp && mv -f $conf.tmp $conf && echo "12.0 MHZ CLock Enabled";;
-    "2")  sed -e 's/\/\/ #define EXTERNAL_OSC 14/ #define EXTERNAL_OSC 14/g' $conf > $conf.tmp && mv -f $conf.tmp $conf && echo "14.4 MHZ CLock Enabled";;
-    "3")  sed -e 's/\/\/ #define EXTERNAL_OSC 19/ #define EXTERNAL_OSC 19/g' $conf > $conf.tmp && mv -f $conf.tmp $conf && echo "19.2 MHZ CLock Enabled";;
-    "4")  sed -e 's/\/\/ #define USE/             #define USE/g'             $conf > $conf.tmp && mv -f $conf.tmp $conf && echo "COS As Lockout Enabled";;
-    "5")  sed -e 's/\/\/ #define ARDUINO_MODE/    #define ARDUINO_MODE/g'    $conf > $conf.tmp && mv -f $conf.tmp $conf && echo "Mode Pins Enabled";;
+    "1")  sed -e 's/\/\/ #define EXTERNAL_OSC 12000000/#define EXTERNAL_OSC 12000000/g' $conf > $conf.tmp && mv -f $conf.tmp $conf && echo "12.0 MHZ CLock Enabled";;
+    "2")  sed -e 's/\/\/ #define EXTERNAL_OSC 14400000/#define EXTERNAL_OSC 14400000/g' $conf > $conf.tmp && mv -f $conf.tmp $conf && echo "14.4 MHZ CLock Enabled";;
+    "3")  sed -e 's/\/\/ #define EXTERNAL_OSC 19200000/#define EXTERNAL_OSC 19200000/g' $conf > $conf.tmp && mv -f $conf.tmp $conf && echo "19.2 MHZ CLock Enabled";;
+    "4")  sed -e 's/\/\/ #define USE_COS_AS_LOCKOUT /#define USE_COS_AS_LOCKOUT/g' $conf > $conf.tmp && mv -f $conf.tmp $conf && echo "COS As Lockout Enabled";;
+    "5")  sed -e 's/\/\/ #define ARDUINO_MODE_PINS/#define ARDUINO_MODE_PINS/g'    $conf > $conf.tmp && mv -f $conf.tmp $conf && echo "Mode Pins Enabled";;
     "6")  mv -f $confbak $conf ;;
-    "Q")  echo "If any changes are made you need to upload to MMDVM" && exit;;
-    "q")  echo "If any changes are made you need to upload to MMDVM" && exit;; 
+    "Q")  echo "If any changes are made you need to (re-)upload the firmware to MMDVM" && exit;;
+    "q")  echo "If any changes are made you need to (re-)upload the firmware to MMDVM" && exit;;
      * )  echo "invalid option"     ;;
     esac
     sleep 1
