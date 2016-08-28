1f7c16c7e8c235d4d9ddadf8d4b4b8b129de445d
Jonathan Naylor <naylorjs@yahoo.com>
2016-08-28 11:49:31 +0100

    Add the basic EMB processing.

---
+++ DMREMB.h
@@ -0,0 +1,33 @@
+/*
+ *   Copyright (C) 2015,2016 by Jonathan Naylor G4KLX
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#if !defined(DMREMB_H)
+#define DMREMB_H
+
+#include "Config.h"
+#include "Globals.h"
+
+class CDMREMB
+{
+public:
+  static uint16_t emb(uint8_t colorCode, bool pi, uint8_t lcss);
+
+private:
+};
+
+#endif
