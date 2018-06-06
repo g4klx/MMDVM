478b0fdb323c083703ddb6e1d4da87d87aa4c5b2
LucaMarche IZ1MLT <lucamarche-iz1mlt@users.noreply.github.com>
2018-06-06 08:23:58 +0200

    Update IO.cpp

--- IO.cpp
+++ IO.cpp
@@ -263,7 +263,7 @@ void CIO::process()
 #if defined(CONSTANT_SRV_LED)
     setLEDInt(true);
 #else
-    if (m_ledCount >= 24000U) {
+    if (m_ledCount >= 48000U) {
       m_ledCount = 0U;
       m_ledValue = !m_ledValue;
       setLEDInt(m_ledValue);
