d66c6d7f9c356caa0d527100bcfe194df88b9cd8
Jonathan Naylor <naylorjs@yahoo.com>
2020-04-26 15:24:51 +0100

    Fix the multiplication.

--- FMCTCSSTX.cpp
+++ FMCTCSSTX.cpp
@@ -106,7 +106,7 @@ uint8_t CFMCTCSSTX::setParams(uint8_t frequency, uint8_t level)
 
   q31_t arg = 0;
   for (uint16_t i = 0U; i < m_length; i++) {
-    q63_t value = ::arm_sin_q31(arg) * q15_t(level * 13);
+    q63_t value = ::arm_sin_q31(arg) * q63_t(level * 13);
     m_values[i] = q15_t(__SSAT((value >> 31), 16));
 
     arg += entry->increment;
