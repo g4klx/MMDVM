458a3d2d240c68d9fbfd304345c4de466d149329
Jonathan Naylor <naylorjs@yahoo.com>
2020-04-26 13:22:16 +0100

    Bump the version date.

--- SerialPort.cpp
+++ SerialPort.cpp
@@ -101,7 +101,7 @@ const uint8_t MMDVM_DEBUG5       = 0xF5U;
 #define	HW_TYPE	"MMDVM"
 #endif
 
-#define DESCRIPTION "20200425 (D-Star/DMR/System Fusion/P25/NXDN/POCSAG/FM)"
+#define DESCRIPTION "20200426 (D-Star/DMR/System Fusion/P25/NXDN/POCSAG/FM)"
 
 #if defined(GITVERSION)
 #define concat(h, a, b, c) h " " a " " b " GitID #" c ""
