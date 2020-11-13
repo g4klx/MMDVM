23ba73cabd97857a2f02077b9e298d993eee1c5f
Jonathan Naylor <naylorjs@yahoo.com>
2020-11-13 15:22:35 +0000

    Disable I2C in M17_AX25_FM.

--- Config.h
+++ Config.h
@@ -108,7 +108,7 @@
 #define SERIAL_REPEATER
 
 // Use the modem as an I2C repeater for OLED displays
-#define I2C_REPEATER
+// #define I2C_REPEATER
 
 // To reduce CPU load, you can remove the DC blocker by commenting out the next line
 #define USE_DCBLOCKER
