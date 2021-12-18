69adaf2ebba5e86cddf53d8e1fee481fbb5111e7
Jonathan Naylor <naylorjs@yahoo.com>
2021-12-18 22:34:24 +0000

    Update SerialTeensy.cpp

--- SerialTeensy.cpp
+++ SerialTeensy.cpp
@@ -1,4 +1,6 @@
 /*
+ *   Copyright (C) 2017 by Chris Huitema
+ *
  *   This program is free software; you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
  *   the Free Software Foundation; either version 2 of the License, or
